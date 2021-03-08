5fcb90e862f569b79824b8d5df8777a9e5df0b93
Munity <38476677+MunityVR@users.noreply.github.com>
2021-03-08 06:45:42 -0600

    Add files via upload

--- buttons.js
+++ buttons.js
@@ -9,11 +9,14 @@ function info() {
     window.location.href='https://www.youtube.com/channel/UCfdNM3NAhaBOXCafH7krzrA';
 }
 function home() {
-    window.location.href='My Webpage.html';
+    window.location.href='index.html';
 }
 function youtube() {
     window.location.href='https://www.youtube.com';
 }
 function channels() {
-    window.location.href='Channels.html';
+    window.location.href='channels.html';
+}
+function about() {
+    window.location.href='about.html';
 }
